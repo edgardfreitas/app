d9f71a1f509ab9de0c347a42f204d2faf9820f9a
Edgard <edgardfp@gmail.com>
2024-09-13 13:01:35 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -62,6 +62,22 @@ const metasRealizadas = async () => {
   });
 };
 
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+    return meta.checked != true;
+  });
+
+  if (abertas.length == 0) {
+    console.log("Não existem metas abertas! :)");
+    return;
+  }
+
+  await select({
+    message: "Metas Abertas " + abertas.length,
+    choices: [...abertas],
+  });
+};
+
 const start = async () => {
   while (true) {
     const opcao = await select({
@@ -80,6 +96,10 @@ const start = async () => {
           value: "realizadas",
         },
         {
+          name: "Metas abertas",
+          value: "abertas",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -96,6 +116,9 @@ const start = async () => {
         break;
       case "realizadas":
         await metasRealizadas();
+        break;
+      case "abertas":
+        await metasAbertas();
         break;
       case "sair":
         console.log("Até a próxima");
