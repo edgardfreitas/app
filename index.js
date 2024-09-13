633400e74c6734f22ae48a2f4a082c4eba9b5a9a
Edgard <edgardfp@gmail.com>
2024-09-13 13:57:36 -0300

    sistema de mensagem

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { select, input, checkbox } = require("@inquirer/prompts");
 
+let mensagem = "Bem Vindo Ao App de Metas";
+
 let meta = {
   value: "Tomar 3L de água por dia",
   checked: false,
@@ -11,11 +13,13 @@ const cadastrarMeta = async () => {
   const meta = await input({ message: "Digite a meta" });
 
   if (meta.length == 0) {
-    console.log("A meta não pode ser vazia");
+    mensagem = "A meta não pode ser vazia";
     return;
   }
 
   metas.push({ value: meta, checked: false });
+
+  mensagem = "Meta cadastrada com sucesso!";
 };
 
 const listarMetas = async () => {
@@ -31,7 +35,7 @@ const listarMetas = async () => {
   });
 
   if (respostas.length == 0) {
-    console.log("Nenhuma meta selecionada");
+    mensagem = "Nenhuma meta selecionada";
     return;
   }
 
@@ -43,7 +47,7 @@ const listarMetas = async () => {
     meta.checked = true;
   });
 
-  console.log("Metas(s) marcadas como concluída(s)");
+  mensagem = "Metas(s) marcada(s) como concluída(s)";
 };
 
 const metasRealizadas = async () => {
@@ -52,7 +56,7 @@ const metasRealizadas = async () => {
   });
 
   if (realizadas.length == 0) {
-    console.log("Não existem metas realizadas! :(");
+    mensagem = "Não existem metas realizadas! :(";
     return;
   }
 
@@ -68,7 +72,7 @@ const metasAbertas = async () => {
   });
 
   if (abertas.length == 0) {
-    console.log("Não existem metas abertas! :)");
+    mensagem = "Não existem metas abertas! :)";
     return;
   }
 
@@ -89,7 +93,7 @@ const deletarMetas = async () => {
   });
 
   if (itemsADeletar == 0) {
-    console.log("Nenhum item para deletar!");
+    mensagem = "Nenhum item para deletar!";
     return;
   }
 
@@ -99,11 +103,23 @@ const deletarMetas = async () => {
     });
   });
 
-  console.log("Meta(s) deletada(s) com sucesso!");
+  mensagem = "Meta(s) deletada(s) com sucesso!";
+};
+
+const mostrarMensagem = () => {
+  console.clear();
+
+  if (mensagem != "") {
+    console.log(mensagem);
+    console.log("");
+    mensagem = "";
+  }
 };
 
 const start = async () => {
   while (true) {
+    mostrarMensagem();
+
     const opcao = await select({
       message: "Menu >",
       choices: [
